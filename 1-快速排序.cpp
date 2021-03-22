9b15b7342b5af4e0d09f3a86cc03d1d1cf1ad143
zcy <chengy_z@163.com>
2021-03-22 16:59:29 +0800

    3.22

---
+++ 1-快速排序.cpp
@@ -0,0 +1,64 @@
+// 数据范围
+// 1≤n≤100000
+// 输入样例：
+// 5
+// 3 1 2 4 5
+// 输出样例：
+// 1 2 3 4 5
+
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+int q[N];
+
+// 写法1
+void quick_sort(int q[], int l, int r) {
+    if (l >= r) return;
+    
+    int x = q[l + r >> 1];// 获取数组的中间数字 
+    int i = l - 1, j = r + 1;// i和j的定义比较重要
+    
+    while (i < j) {
+        do i ++; while (q[i] < x);
+        do j --; while (q[j] > x);
+        if (i < j) swap(q[i], q[j]);
+    }
+    
+    quick_sort(q, l, j);
+    quick_sort(q, j + 1, r);
+}
+
+
+// 写法2
+void quick_sort2(int q[], int l, int r) {
+    if (l >= r) return;
+    
+    int i = l, j = r;
+    int x = q[l + r >> 1];
+    
+    while (i < j) {
+        while (q[i] < x) i ++;
+        while (q[j] > x) j --;
+        if (i < j) swap(q[i], q[j]);
+    }
+    
+    quick_sort2(q, l, j);
+    quick_sort2(q, j + 1, r);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    
+    // 输入数字
+    for (int i = 0; i < n; i ++) cin >> q[i];
+    
+    quick_sort2(q, 0, n - 1);
+    
+    for (int i = 0; i < n; i ++) cout << q[i] <<' ';
+    cout << endl;
+    
+    return 0;
+}
